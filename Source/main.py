244341d792e93426f2d6641bf32c255837f7f1a3
ahsanirfan961 <116998027+ahsanirfan961@users.noreply.github.com>
2024-01-15 23:53:16 +0500

    Bug Fixes

--- Source/main.py
+++ Source/main.py
@@ -1,12 +1,14 @@
 from package.Welcome import Welcome
 import sys 
 from package.data import widget, app, sqlManager
+from PyQt5.QtGui import QIcon
 
 
 welcome = Welcome() 
 widget.addWidget(welcome)
 widget.setFixedHeight(700)
 widget.setFixedWidth(1200) 
+widget.setWindowIcon(QIcon('Images/Logo.png'))
 widget.show()
 
 try: 
